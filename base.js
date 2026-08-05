ac058bbe6c082e1026a3d234041c0acc51b947fe
LucianoEsquivel <lucianostivy@gmail.com>
2026-08-05 14:37:30 -0300

    14.37

--- base.js
+++ base.js
@@ -39,33 +39,33 @@ const baseDatosPromos = [
     },
 
     // ==========================================
-    // 3. NAVE - GALICIA (3 y 6 Cuotas)
+    // 3. NAVE - GALICIA (3, 6, 9 y 12 Cuotas)
     // ==========================================
     {
         plataforma: "nave",
         cuotas: 3,
-        recargo_porcentaje: 4.40, 
+        recargo_porcentaje: 4.90, 
         recargo_minimo: 0.00 
     },
 
     {
         plataforma: "nave",
         cuotas: 6,
-        recargo_porcentaje: 7.30, 
+        recargo_porcentaje: 8.40, 
         recargo_minimo: 0.00 
     },
 
     {
         plataforma: "nave",
         cuotas: 9,
-        recargo_porcentaje: 10.40, 
+        recargo_porcentaje: 12.40, 
         recargo_minimo: 0.00 
     },
 
     {
         plataforma: "nave",
         cuotas: 12,
-        recargo_porcentaje: 12.80, 
+        recargo_porcentaje: 15.60, 
         recargo_minimo: 0.00 
     },
 ];
@@ -75,14 +75,9 @@ const baseDatosPromos = [
 // ==========================================
 const baseDatosNovedades = [
     {
-        fecha: "29/06/2026",
-        titulo: "Payway",
-        descripcion: "Payway bajo su tasa directa en todos sus plazos"
+        fecha: "05/08/2026",
+        titulo: "Nave",
+        descripcion: "Nave aumento las tasas de recargo para 3, 6, 9 y 12 cuotas.",
     },
     
-    {
-        fecha: "29/06/2026",
-        titulo: "Getnet",
-        descripcion: "Getnet bajo su tasa directa en todos sus plazos"
-    }
 ];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'simulador-v1.1.6';
+const CACHE_NAME = 'simulador-v1.1.8';
 const ASSETS = [
   './',
   './index.html',
